56d7c13c15a1f4ad721602d16db817f1f25486f0
aleksandr.orlov <aleksandr.orlov@travelline.ru>
2023-08-23 10:09:48 +0300

    init commit

---
+++ ConsoleApp1/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace ConsoleApp1;
+internal class ApplicationContext : DbContext
+{
+    public DbSet<User> Users => Set<User>();
+    public ApplicationContext()
+    {
+        Database.EnsureDeleted();
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring( DbContextOptionsBuilder optionsBuilder )
+    {
+        optionsBuilder.UseSqlite( "Data Source=helloapp.db" );
+        optionsBuilder.LogTo( Console.WriteLine, new[] { RelationalEventId.CommandExecuted } );
+    }
+}
---
+++ ConsoleApp1/Examples/Decoder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Examples;
+internal class Decoder
+{
+    private readonly int _step;
+    private const int AlphabetSize = 33;
+
+    public Decoder(int step)
+    {
+        _step = step;
+    }
+
+    public string Decode(string input)
+    {
+        return String.Empty;
+    }
+}
